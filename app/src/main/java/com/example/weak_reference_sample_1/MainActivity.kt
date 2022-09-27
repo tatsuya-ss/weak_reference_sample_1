15a91bb74804014639cfdbffc3020afb64659de6
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-09-28 08:24:30 +0900

    softReferenceで解放してみる

--- app/src/main/java/com/example/weak_reference_sample_1/MainActivity.kt
+++ app/src/main/java/com/example/weak_reference_sample_1/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.weak_reference_sample_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.ref.SoftReference
 
 class MainActivity : AppCompatActivity() {
+
+    private var number: Int? = 1
+    private var softReference: SoftReference<Int>? = SoftReference(1)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var i = softReference?.get()
+        Log.d("Tatsuya", "初回呼び出し: ${i}")
+
+        number = null
+        i = null
+
+        i = softReference?.get()
+        Log.d("Tatsuya", "開放後の呼び出し: ${i}")
     }
 }
